7aa4aef121d7109ee399cedf204ea64b2f0ddfc5
karansamant <141988116+karansamant@users.noreply.github.com>
2024-06-24 11:58:31 +0530

    first

---
+++ app/components/meals/meals-form-submit.js
@@ -0,0 +1,9 @@
+import { useFormStatus } from 'react-dom';
+
+export default function MealsFormSubmit() {
+    const status = useFormStatus();
+
+    return <button disabled={status.pending}>
+        {status.pending ? 'Submitting..' : 'Share Meal'}
+    </button>
+}
